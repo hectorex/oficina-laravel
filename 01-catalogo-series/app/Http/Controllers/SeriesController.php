83afaae596f198313aab440660a733c9b7cd382f
hectorex <celsohectorhm@gmail.com>
2026-09-07 18:02:15 -0400

    [catalogo-series] Cria x-layout component para reutilização de estrutura HTML

--- 01-catalogo-series/app/Http/Controllers/SeriesController.php
+++ 01-catalogo-series/app/Http/Controllers/SeriesController.php
@@ -14,6 +14,6 @@ class SeriesController extends Controller
             'Ricky And Morty',
         ];
 
-        return view('listar-series')->with('series', $series);
+        return view('series.index')->with('series', $series);
     }
 }
