8547ef5ffffe0f216be7ced1411daec78513a10f
hectorex <celsohectorhm@gmail.com>
2026-09-09 11:00:36 -0400

    [catalogo-series] Organiza rotas com Route::resource

--- 01-catalogo-series/routes/web.php
+++ 01-catalogo-series/routes/web.php
@@ -4,9 +4,7 @@ use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return to_route('series.index');
 });
 
-Route::get('/series', [SeriesController::class, 'index']);
-Route::get('/series/criar', [SeriesController::class, 'create']);
-Route::post('/series/salvar', [SeriesController::class, 'store']);
+Route::resource('/series', SeriesController::class);
