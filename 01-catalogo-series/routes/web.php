61d6e34d593496d4493534b1fca7d890ffaf7ee1
hectorex <celsohectorhm@gmail.com>
2026-09-09 12:18:14 -0400

    [catalogo-series] Adiciona exclusão de séries e restringe rotas do resource

--- 01-catalogo-series/routes/web.php
+++ 01-catalogo-series/routes/web.php
@@ -7,4 +7,5 @@ Route::get('/', function () {
     return to_route('series.index');
 });
 
-Route::resource('/series', SeriesController::class);
+Route::resource('/series', SeriesController::class)
+    ->only(['index','create','store', 'destroy']);
