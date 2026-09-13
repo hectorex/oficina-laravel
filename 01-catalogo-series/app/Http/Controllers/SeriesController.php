6a59ec83695bce2c4390f6e99db542a1cc83c388
hectorex <celsohectorhm@gmail.com>
2026-09-13 15:25:37 -0400

    [catalogo-series] Extrai validação para SeriesFormRequest com mensagens customizadas

--- 01-catalogo-series/app/Http/Controllers/SeriesController.php
+++ 01-catalogo-series/app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\SeriesFormRequest;
 use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -22,12 +23,8 @@ class SeriesController extends Controller
         return view('series.create');
     }
 
-    public function store(Request $request)
+    public function store(SeriesFormRequest $request)
     {
-        $request->validate([
-            'nome' => ['required', 'min:3']
-        ]);
-
         $serie = Serie::create($request->all());
         return to_route('series.index')
         ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
@@ -46,7 +43,7 @@ class SeriesController extends Controller
         return view('series.edit')->with('serie', $series);
     }
 
-    public function update(Serie $series, Request $request)
+    public function update(Serie $series, SeriesFormRequest $request)
     {   
         $old_name = $series->nome;
         $series->fill($request->all());
