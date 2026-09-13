b778d4ed6d970d6d740a9e2d5cba6ce4589e484e
hectorex <celsohectorhm@gmail.com>
2026-09-13 13:49:42 -0400

    [catalogo-series] Adiciona mensagem de sucesso via flash session ao criar e excluir séries

--- 01-catalogo-series/app/Http/Controllers/SeriesController.php
+++ 01-catalogo-series/app/Http/Controllers/SeriesController.php
@@ -8,11 +8,13 @@ use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = Serie::query()->orderBy('nome')->get();
-
-        return view('series.index')->with('series', $series);
+        $mensagemSucesso = session('mensagem.sucesso');
+        
+        return view('series.index')->with('series', $series)
+            ->with('mensagemSucesso', $mensagemSucesso);
     }
 
     public function create()
@@ -23,13 +25,14 @@ class SeriesController extends Controller
     public function store(Request $request)
     {
         Serie::create($request->all());
-
+        $request->session()->flash('mensagem.sucesso', 'Série adicionada com sucesso');
         return to_route('series.index');
     }
 
     public function destroy(Request $request) 
     {
         Serie::destroy($request->series);
+        $request->session()->flash('mensagem.sucesso', 'Série removida com sucesso');
 
         return to_route('series.index');
     }
